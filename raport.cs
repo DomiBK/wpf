acd718dffd0e43c7dd22a086626073f9ff1183ac
DomiBK <148908251+DomiBK@users.noreply.github.com>
2025-04-24 12:24:17 +0200

    Update raport.cs

--- raport.cs
+++ raport.cs
@@ -1,1 +1,12 @@
+public class ElementRaportu
+{
+    public string Nazwa { get; set; }
+    public string Szczegoly { get; set; }
+}
 
+public class Raport
+{
+    public string Tytul { get; set; }
+    public DateTime DataStworzenia { get; set; }
+    public List<ElementRaportu> Elementy { get; set; } = new();
+}
